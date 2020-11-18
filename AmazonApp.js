c28c5521a8f2b7d10da25050815a863bddb23420
Md Siamul Islam <siam.mhorizon@gmail.com>
2020-11-17 22:56:05 -0600

    Setup Home Page and added components: header and home; created components and searchbar, ++

---
+++ AmazonApp.js
@@ -0,0 +1,31 @@
+// npx create-react-app amazon_clone
+// create git repository and setup project repo
+
+// Firebase.com  allows: database, host the App online, Cloud Functions,... -create project Amazon-Clone-App
+// connect to Stripe: for processing payments
+// Grab config file from Firebase -object from firebase that will allow to connect the frontend of React app with the Firebase
+
+// Firebase app nickname amazon-clone-app
+// firebase hosting : clone-app-807ea
+// npm install -g firebase-tools
+// goto project settings in firebase webpage -> click config -> copy provided code
+// create new file under src "firebase.js" -> paste copied snippet under that new file
+
+
+// Part 2
+// Home Page
+// clean test files and logo -> app.test.js, logo.svg, setuptests.js
+// clear out header import logo, and the code under app div in App.js and create h1 ->save ->refresh react page
+
+// clear out app.css and remove invisible margin in index.css 
+// BEM convention under App.js
+
+// header and home components 
+// create Header.js file with 'H' in caps
+
+// use ES7 snippets -extensions
+// eg: RFCE in header automatically inserts functional component snippet
+// also use extension: prettier ->settings ctrl + , ->check format on save
+// autosave afterDelay 500ms
+
+
